054c36f918d79f024040539b3159a1c8e4dcb84d
hliangzhao <hliangzhao@zju.edu.cn>
2020-10-18 18:53:54 +0800

    Finish optimization

--- code/my_utils.py
+++ code/my_utils.py
@@ -9,6 +9,7 @@ import torchvision.transforms as transforms
 import random
 import zipfile
 import math
+import numpy as np
 
 
 def use_svg_display():
@@ -514,3 +515,108 @@ def rnn_train_and_predict_torch(model, num_hiddens, vocab_size, device,
                 print(' -', rnn_predict_torch(
                     prefix, pred_len, model, vocab_size, device, idx_to_char,
                     char_to_idx))
+
+
+def train_2d(trainer):
+    x1, x2, s1, s2 = -5, -2, 0, 0
+    results = [(x1, x2)]
+    for i in range(20):
+        x1, x2, s1, s2 = trainer(x1, x2, s1, s2)
+        results.append((x1, x2))
+    print('epoch %d, x1 %f, x2 %f' % (i + 1, x1, x2))
+    return results
+
+
+def show_trace_2d(f, results):
+    set_figsize()
+    use_svg_display()
+    plt.plot(*zip(*results), '-o', color='#ff7f0e')
+    x1, x2 = np.meshgrid(np.arange(-5.5, 1.0, 0.1), np.arange(-3.0, 1.0, 0.1))
+    plt.contour(x1, x2, f(x1, x2), colors='#1f77b4')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+
+
+def get_NASA_data():
+    data = np.genfromtxt('../data/airfoil_self_noise.dat', delimiter='\t')
+    data = (data - data.mean(axis=0)) / data.std(axis=0)
+    # 前1500个样本(每个样本5个特征)
+    return torch.tensor(data[:1500, :-1], dtype=torch.float32), torch.tensor(data[:1500, -1], dtype=torch.float32)
+
+
+def sgd(params, states, hyperparams):
+    for p in params:
+        p.data -= hyperparams['lr'] * p.grad.data
+
+
+def opt_train(optimizer_fn, states,  hyperparams, features, labels, batch_size=10, num_epochs=2):
+    net, loss = linreg, squared_loss
+    w = torch.nn.Parameter(
+        torch.tensor(np.random.normal(0, 0.01, size=(features.shape[1], 1)), dtype=torch.float32),
+        requires_grad=True
+    )
+    b = torch.nn.Parameter(torch.zeros(1, dtype=torch.float32), requires_grad=True)
+    
+    def eval_loss():
+        return loss(net(features, w, b), labels).mean().item()
+    
+    ls = [eval_loss()]
+    data_iter = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(features, labels), batch_size, shuffle=True)
+    
+    for _ in range(num_epochs):
+        start = time.time()
+        for batch_i, (X, y) in enumerate(data_iter):
+            l = loss(net(X, w, b), y).mean()  # 使用平均损失
+
+            # 梯度清零
+            if w.grad is not None:
+                w.grad.data.zero_()
+                b.grad.data.zero_()
+
+            l.backward()
+            optimizer_fn([w, b], states, hyperparams)  # 迭代模型参数
+            if (batch_i + 1) * batch_size % 100 == 0:
+                ls.append(eval_loss())  # 每100个样本记录下当前训练误差
+    
+    # 打印结果和作图
+    print('loss: %f, %f sec per epoch' % (ls[-1], time.time() - start))
+    set_figsize()
+    use_svg_display()
+    plt.plot(np.linspace(0, num_epochs, len(ls)), ls)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+
+
+def opt_train_torch(optimizer_fn, optimizer_hyperparams, features, labels, batch_size=10, num_epochs=2):
+    # 初始化模型
+    net = nn.Sequential(
+        nn.Linear(features.shape[-1], 1)
+    )
+    loss = nn.MSELoss()
+    optimizer = optimizer_fn(net.parameters(), **optimizer_hyperparams)
+    
+    def eval_loss():
+        # 除以2是为了和opt_train()保持一致, 因为squared_loss中除了2
+        return loss(net(features).view(-1), labels).item() / 2
+    
+    ls = [eval_loss()]
+    data_iter = torch.utils.data.DataLoader(
+        torch.utils.data.TensorDataset(features, labels), batch_size, shuffle=True)
+
+    for _ in range(num_epochs):
+        start = time.time()
+        for batch_i, (X, y) in enumerate(data_iter):
+            l = loss(net(X).view(-1), y) / 2 
+
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            if (batch_i + 1) * batch_size % 100 == 0:
+                ls.append(eval_loss())
+    # 打印结果和作图
+    print('loss: %f, %f sec per epoch' % (ls[-1], time.time() - start))
+    set_figsize()
+    use_svg_display()
+    plt.plot(np.linspace(0, num_epochs, len(ls)), ls)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
